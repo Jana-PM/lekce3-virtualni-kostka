aeae0556b5a3595e9402d6f895d921a909c4feac
Jana Maskova <jana.maskova93@gmail.com>
2022-02-28 21:02:09 +0100

    Zmena nazvu slozky obrazky

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // tady je místo pro náš program
+function hod(){
+   let cislo = Math.ceil( Math.random (0,1)*6);
+}
+
+const kostka= getElementById=("kostka");
+kostka.src = "obrazky/" 
+
+
+if (cislo===6){
+    document.querySelector("#zprava").innerHtml = "Gratuluji, vzhrál jsi!"
+
+}
+
+else if {
+
+}
+
+
